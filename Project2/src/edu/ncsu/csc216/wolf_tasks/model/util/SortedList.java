f3d20a5f98f27b8c211e4c0147477409e935759b
riley <riley@DESKTOP-3QBIQEM>
2021-04-05 12:59:37 -0400

    [Implementation] Finished skeleton code for util package

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SortedList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SortedList.java
@@ -1,35 +1,89 @@
 package edu.ncsu.csc216.wolf_tasks.model.util;
 
-public class SortedList implements ISortedList{
+public class SortedList<E extends Comparable<E>> implements ISortedList{
 
+	/** The front of the list */
+	ListNode front;
+	/** The size of the list */
+	int size;
+	
+	/**
+	 * Adds the element to the list in sorted order.
+	 * @param element element to add
+	 * @throws NullPointerException if element is null
+	 * @throws IllegalArgumentException if element cannot be added 
+	 */
 	@Override
 	public void add(Comparable element) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Returns the element from the given index.  The element is
+	 * removed from the list.
+	 * @param idx index to remove element from
+	 * @return element at given index
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
+	 */
 	@Override
 	public Comparable remove(int idx) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Returns true if the element is in the list.
+	 * @param element element to search for
+	 * @return true if element is found
+	 */
 	@Override
 	public boolean contains(Comparable element) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Returns the element at the given index.
+	 * @param idx index of the element to retrieve
+	 * @return element at the given index
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
+	 */
 	@Override
 	public Comparable get(int idx) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Returns the number of elements in the list.
+	 * @return number of elements in the list
+	 */
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * The ListNode class represents a single element in a SortedList
+	 * @author rsthoma5
+	 *
+	 */
+	private class ListNode {
+		/**The data the ListNode is holding */
+		private E data;
+		/**The next node in the list */
+		private ListNode next;
+		/**
+		 * Constructs a ListNode with the given data and next ListNode
+		 * @param data the data to set for the ListNode
+		 * @param next the next ListNode in the list
+		 */
+		private ListNode(E data, ListNode next) {
+			
+		}
+	}
 }
--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
@@ -1,53 +1,125 @@
 package edu.ncsu.csc216.wolf_tasks.model.util;
 
-public class SwapList implements ISwapList{
+/**
+ * SwapList is a customized ArrayList with added functionality for moving items up and down 
+ * @author rsthoma5
+ * @param <E> type for the SwapList
+ */
+public class SwapList<E> implements ISwapList{
 
-	@Override
-	public void add(Object element) {
-		// TODO Auto-generated method stub
-		
+	/** The initial capacity of a SwapList */
+	private static final int INITIAL_CAPACITY = 10;
+	/** The list holding the swap lists information */
+	private E[] list;
+	/** The size of the list */
+	private int size;
+	/**
+	 * Constructs an empty SwapList of generic type E
+	 */
+	@SuppressWarnings("unchecked")
+	public SwapList() {
+		list = (E[]) new Object[INITIAL_CAPACITY];
+		size = 0;
 	}
 
+	/**
+	 * Returns the element from the given index.  The element is
+	 * removed from the list.
+	 * @param idx index to remove element from
+	 * @return element at given index
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
+	 */
 	@Override
 	public Object remove(int idx) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Moves the element at the given index to index-1.  If the element is
+	 * already at the front of the list, the list is not changed.
+	 * @param idx index of element to move up
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
+	 */
 	@Override
 	public void moveUp(int idx) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Moves the element at the given index to index+1.  If the element is
+	 * already at the end of the list, the list is not changed.
+	 * @param idx index of element to move down
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
+	 */
 	@Override
 	public void moveDown(int idx) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Moves the element at the given index to index 0.  If the element is
+	 * already at the front of the list, the list is not changed.
+	 * @param idx index of element to move to the front
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
+	 */
 	@Override
 	public void moveToFront(int idx) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Moves the element at the given index to size-1.  If the element is
+	 * already at the end of the list, the list is not changed.
+	 * @param idx index of element to move to the back
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
+	 */
 	@Override
 	public void moveToBack(int idx) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Returns the element at the given index.
+	 * @param idx index of the element to retrieve
+	 * @return element at the given index
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
+	 */
 	@Override
-	public Object get(int idx) {
+	public E get(int idx) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Returns the number of elements in the list.
+	 * @return number of elements in the list
+	 */
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	/**
+	 * Adds the element to the end of the list.
+	 * @param element element to add
+	 * @throws NullPointerException if element is null
+	 * @throws IllegalArgumentException if element cannot be added 
+	 */
+	@Override
+	public void add(Object element) {
+		if(element == null) {
+			throw new IllegalArgumentException("Cannot add null element");
+		}
+	}
 }
