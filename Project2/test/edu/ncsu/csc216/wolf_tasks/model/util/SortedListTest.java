3ef08058ff134788cb5e561b5ba74cda42f4da8f
riley <riley@DESKTOP-3QBIQEM>
2021-04-05 18:02:00 -0400

    [Test] Completed skeleton code for util test package

---
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SortedListTest.java
@@ -0,0 +1,29 @@
+package edu.ncsu.csc216.wolf_tasks.model.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SortedListTest {
+	/**
+	 * Tests that the SortedList constructor works correctly
+	 */
+	@Test
+	public void testSortedList() {
+		fail("Not yet implemented");
+	}
+	/**
+	 * Tests that add correctly adds an element to the sortedList
+	 */
+	@Test
+	public void testAdd() {
+		
+	}
+	/**
+	 * Tests that remove correctly removes the element at the given index
+	 */
+	@Test
+	public void testRemove() {
+		
+	}
+}
---
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/util/SwapListTest.java
@@ -0,0 +1,36 @@
+package edu.ncsu.csc216.wolf_tasks.model.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SwapListTest {
+	/**
+	 * Tests that the SwapList constrcutor works correctly
+	 */
+	@Test
+	public void testSwapList() {
+		fail("Not yet implemented");
+	}
+	/**
+	 * Tests that the add method correctly adds the given element
+	 */
+	@Test
+	public void testAdd() {
+		
+	}
+	/**
+	 * Tests that the remove method correctly removes the element at the given index
+	 */
+	@Test
+	public void testRemove() {
+		
+	}
+	/**
+	 * Tests that all the move methods work correctly
+	 */
+	@Test
+	public void testMove() {
+		
+	}
+}
