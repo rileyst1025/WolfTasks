0b2d66e36f3183d9ae5ad44ae62fc32cbcc268c8
riley <riley@DESKTOP-3QBIQEM>
2021-04-05 17:29:32 -0400

    [Test] Created test folder and skeleton io tests

---
+++ Project2/test/edu/ncsu/csc216/wold_tasks/model/io/NotebookReaderTest.java
@@ -0,0 +1,16 @@
+package edu.ncsu.csc216.wold_tasks.model.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NotebookReaderTest {
+	/**
+	 * Tests that readNotebookFile correctly reads a Notebook from a file
+	 */
+	@Test
+	public void testReadNotebookFile() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Project2/test/edu/ncsu/csc216/wold_tasks/model/io/NotebookWriterTest.java
@@ -0,0 +1,16 @@
+package edu.ncsu.csc216.wold_tasks.model.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NotebookWriterTest {
+	/**
+	 * Tests that writeNotebookFile correctly writes a Notebook to the correct file
+	 */
+	@Test
+	public void testWriteNotebookFile() {
+		fail("Not yet implemented");
+	}
+
+}
