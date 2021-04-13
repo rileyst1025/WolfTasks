3af8fc7b8077268e136f9c0d3da31aeaeccb17f4
riley <riley@DESKTOP-3QBIQEM>
2021-04-13 06:06:49 -0400

    [Debug] Final error message fixes for Task

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/Task.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/tasks/Task.java
@@ -50,7 +50,7 @@ public class Task implements Cloneable {
 	 */
 	public void setTaskName(String taskName) {
 		if(taskName == null || "".equals(taskName)) {
-			throw new IllegalArgumentException("Incomplete task information");
+			throw new IllegalArgumentException("Incomplete task information.");
 		}
 		this.taskName = taskName;
 	}
@@ -69,7 +69,7 @@ public class Task implements Cloneable {
 	 */
 	public void setTaskDescription(String taskDescription) {
 		if(taskDescription == null || "".equals(taskDescription)) {
-			throw new IllegalArgumentException("Incomplete task information");
+			throw new IllegalArgumentException("Incomplete task information.");
 		}
 		this.taskDescription = taskDescription;
 	}
